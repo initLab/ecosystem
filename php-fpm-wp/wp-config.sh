b58d3eccf48dc730460c163403164c62c57aee37
Alex Milanov <alex@i4web.biz>
2019-06-30 22:16:33 +0300

    fauna relative path fix, php-fpm-wp added plugins

--- php-fpm-wp/wp-config.sh
+++ php-fpm-wp/wp-config.sh
@@ -3,3 +3,7 @@ wp config create --allow-root --dbhost=mysql --dbname=${MYSQL_DATABASE} --dbuser
 wp db create --allow-root
 wp core install --allow-root --url=localhost --title="InitLab" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
 wp theme activate typecore --allow-root
+# plugins
+# wp plugin install https://downloads.wordpress.org/plugin/loco-translate.2.3.0.zip --activate --allow-root
+wp plugin install loco-translate --activate --allow-root
+wp plugin install contact-form-7 --activate --allow-root
